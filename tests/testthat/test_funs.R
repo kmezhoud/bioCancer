baa2e40f59a8592b1b7e7a42c07cfa7e91da377f
kmezhoud <kmezhoud@gmail.com>
2017-10-29 21:36:48 +0100

    test

--- tests/testthat/test_funs.R
+++ tests/testthat/test_funs.R
@@ -16,9 +16,9 @@ testthat::test_that("ReactomeFI connection",
                       checkEq = function(a,b) { if (identical(a,b)) "OK\n" else "FAILED!\n" }
                       source(paste0(system.file(package = "bioCancer"), "/app/tools/bioCancer/getReactomeFUN.R"),
                              encoding = "UTF-8", local = TRUE)
-                      install.packages('Rcurl')
-                      install.packages('XML')
-
+                      # install.packages('RCurl')
+                      # install.packages('XML')
+                      # library('Rcurl', 'XML')
                       ReactomeResult <- queryBuildNetwork(2013, genes = c("TP53","BRCA1"))
                       cat('ReactomeFI connection... ',
                           checkEq(colnames(ReactomeResult), c("first.protein","second.protein"))
